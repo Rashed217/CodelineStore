9fbb888badd3ed5d9a06f217d5b89508e5027222
Mohammed <Mohammed.almanwari.01@gmail.com>
2025-01-22 18:25:39 +0400

    add product service

---
+++ Services/IProductService1.cs
@@ -0,0 +1,14 @@
+﻿using CodelineStore.Data.Model;
+
+namespace CodelineStore.Services
+{
+    public interface IProductService1
+    {
+        int DeleteProduct(int id);
+        List<Product> GetAllProducts();
+        Product GetByID(int id);
+        Product GetByName(string name);
+        List<Product> GetPriceRange(decimal minPrice, decimal maxPrice);
+        int UpdateProduct(Product pr);
+    }
+}
---
+++ Services/ProductService1.cs
@@ -0,0 +1,45 @@
+﻿using CodelineStore.Data.Model;
+using CodelineStore.Data.Repositories;
+
+namespace CodelineStore.Services
+{
+    public class ProductService1 : IProductService1
+    {
+        private readonly IProductRepository1 _repository;
+        public ProductService1(IProductRepository1 repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _repository.GetAllProducts();
+        }
+
+        public Product GetByID(int id)
+        {
+            return _repository.GetByID(id);
+        }
+
+        public Product GetByName(string name)
+        {
+            return _repository.GetByName(name);
+
+        }
+
+        public List<Product> GetPriceRange(decimal minPrice, decimal maxPrice)
+        {
+            return _repository.GetPriceRange(minPrice, maxPrice);
+        }
+
+        public int UpdateProduct(Product pr)
+        {
+            return _repository.UpdateProduct(pr);
+        }
+
+        public int DeleteProduct(int id)
+        {
+            return _repository.DeleteProduct(id);
+        }
+    }
+}
