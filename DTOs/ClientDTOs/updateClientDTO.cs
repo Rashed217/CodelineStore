1326d721d96fa7a7440d5906b8f4d4c0d6cbcba9
Rashed Al Ghailani <Authority217@gmail.com>
2025-01-23 10:59:20 +0400

    Added a page to show user profile

--- DTOs/ClientDTOs/updateClientDTO.cs
+++ DTOs/ClientDTOs/updateClientDTO.cs
@@ -1,9 +1,12 @@
-﻿namespace CodelineStore.DTOs.ClientDTOs
+﻿using CodelineStore.DTOs.UserDTOs;
+
+namespace CodelineStore.DTOs.ClientDTOs
 {
-    public class updateClientDTO
+    public class updateClientDTO : UserInput
     {
         public int? ClientId { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
+        public UserInput User { get; set; }
     }
 }
