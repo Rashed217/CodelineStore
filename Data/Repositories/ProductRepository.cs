fab6e6a4502d94c1b9494bf7c7df2f5638701664
zubair Albulushi <zubair.albu@gmail.com>
2025-01-28 17:02:44 +0400

    Added 124 pic for products

--- Data/Repositories/ProductRepository.cs
+++ Data/Repositories/ProductRepository.cs
@@ -15,7 +15,7 @@ namespace CodelineStore.Data.Repositories
 
         public List<Product> GetAllProductsAsync()
         {
-            return  _context.Products
+            return _context.Products
                 .Include(p => p.Seller)
                 .Include(p => p.Category)
                 .ToList();
@@ -45,7 +45,7 @@ namespace CodelineStore.Data.Repositories
                     ProductId = p.PId,
                     Name = p.Name,
                     Price = p.Price,
-                    MainImagePath = p.Image
+                    Image = p.Image
                 })
                 .FirstOrDefaultAsync();
 
